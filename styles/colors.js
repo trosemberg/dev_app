47810d4aed7d1901ecadde664306d234f06fefa2
Tomás Rosemberg (trosemberg) <tomasroro@hotmail.com>
2018-10-02 02:24:32 -0300

    start

---
+++ styles/colors.js
@@ -0,0 +1,8 @@
+export default{
+    black: 'rgb(40,40,40)',
+    blackBorder: 'rgba(40,40,40,0.6)',
+    greenButton: 'rgba(60,200,80,0.3)',
+    green: 'rgb(60,200,80)',
+    white: 'rgb(220,220,230)',
+    yellow:'rgb(220,220,40)',
+};
